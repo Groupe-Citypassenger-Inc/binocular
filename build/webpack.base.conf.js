4ababefb83642d5fd6a0cd992624b663ad410911
Doe <john.doe@nan.com>
2020-07-17 16:44:58 -0400

    Remove ora spinner & autoprefixer

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -69,11 +69,6 @@ module.exports = {
     ]
   },
   vue: {
-    loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['last 2 versions']
-      })
-    ]
+    loaders: utils.cssLoaders({ sourceMap: useCssSourceMap })
   }
 }
